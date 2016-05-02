511da57cf705c6669b7f47044168366303671538
jake waters <jerkey@g-cipher.net>
2016-05-02 01:12:05 -0700

    add AC Phase control for relay coil current to code

--- src/minievse.ino
+++ src/minievse.ino
@@ -9,6 +9,10 @@
 #define AC_SENSE_2      A5 // measures the voltage on leg 2 of the AC
 #define LED_DATA        6 // PCINT22/0C0A/AIN0/PD6 DIP pin 12, digital 6
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define TRIAC_PULSEWIDTH 4   //trigger pulse width (counts)
+
 void setup() {
   Serial.begin(57600);
   Serial.println("minievse v1.0");
@@ -23,4 +27,57 @@ void loop() {
   Serial.print("     AMP_KNOB: ");
   Serial.print(analogRead(AMP_KNOB));
   delay(100);
+}
+
+void initTriacTimer() {
+// Relay Coil Triac Control Timing Sequence
+// * timer is set up but disabled
+// * zero crossing detected on pin 2
+// * timer starts counting from zero
+// * comparator set to "delay to on" value
+// * counter reaches comparator value
+// * comparator ISR turns on triac gate
+// * counter set to overflow - pulse width
+// * counter reaches overflow
+// * overflow ISR truns off triac gate
+// * triac stops conducting at next zero cross
+
+// The hardware timer runs at 16MHz. Using a
+// divide by 256 on the counter each count is
+// 16 microseconds.  1/2 wave of a 60Hz AC signal
+// is about 520 counts (8,333 microseconds).
+
+  //(see ATMEGA 328 data sheet pg 134 for more details)
+  OCR1A = 100;      //initialize the comparator
+  TIMSK1 = 0x03;    //enable comparator A and overflow interrupts
+  TCCR1A = 0x00;    //timer control registers set for
+  TCCR1B = 0x00;    //normal operation, timer disabled
+  //IRQ0 is pin 2. Call zeroCrossingInterrupt
+  attachInterrupt(0,zeroCrossingInterrupt, FALLING);
+}
+
+//Interrupt Service Routines
+void zeroCrossingInterrupt(){ //zero cross detect
+  TCCR1B=0x04; //start timer with divide by 256 input
+  TCNT1 = 0;   //reset timer - count from zero
+}
+
+ISR(TIMER1_COMPA_vect){ //comparator match
+  digitalWrite(RELAY_TRIAC,HIGH);  //set triac gate to high
+  TCNT1 = 65536-TRIAC_PULSEWIDTH;      //trigger pulse width
+}
+
+ISR(TIMER1_OVF_vect){ //timer1 overflow
+  digitalWrite(RELAY_TRIAC,LOW); //turn off triac gate
+  TCCR1B = 0x00;          //disable timer stopd unintended triggers
+}
+
+void setRelayState(boolean relayState) {
+
+int i=483;
+
+OCR1A = i;     //set the compare register brightness desired.
+if (i<65){i=483;}
+delay(15);
+
 }
